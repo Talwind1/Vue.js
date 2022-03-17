4e8923b785fc31beb59b8adc1daca214f5e20d55
Talwind1 <talwind1@gmail.com>
2022-03-17 10:37:27 +0200

    hello world task

---
+++ notes.js
@@ -0,0 +1,27 @@
+//vue.js is a framework
+//declerative
+//legible
+//easy to maintain
+//powerful
+//best of best (collection of best)
+//elegant
+
+//!directives;
+//v-model: creates a relationshup between the data in the instance / component and a form input (dynamically update values)
+//v-show: showing and hiding the element depend on the conditional
+//v-if: conditional that display the information depends on meeting the requirement(could be any element.)(nounting the element each time-- really expansive)
+//<li v-for = "num in 5" : key = "num">
+//v-bind:  (or : instead!!!) for reusing classes and styles(!), creating dynamic props,etc..
+//:class="red" - example for v-bind
+
+//v-once
+//v-pre
+
+//v-on="click" //@click
+
+//v-html
+//v-text
+
+//modifiers:
+//v-model.trim
+//v-mpdel.number
